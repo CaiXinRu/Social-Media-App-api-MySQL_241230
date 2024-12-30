c68eebf09390f3ed306a630e496b5c2b7fcdfcd0
naomi <naomi@program.com.tw>
2024-12-30 09:56:08 +0800

    feat: connect to MySQL

---
+++ index.js
@@ -0,0 +1,34 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
+const app = express();
+
+// middlewares
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+
+  next();
+});
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+
+app.listen(8800, () => {
+  console.log("Server is running on port 8800");
+});
