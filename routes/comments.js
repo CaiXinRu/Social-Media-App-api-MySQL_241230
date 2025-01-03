62b1b68a81d122cf91f805a475e6f6d2ff61752d
naomi <naomi@program.com.tw>
2025-01-03 09:40:16 +0800

    chore: Adjust routes

--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,7 @@ import { addComment, getComments } from "../controllers/comment.js";
 
 const router = express.Router();
 
-router.get("", getComments);
-router.post("", addComment);
+router.get("/", getComments);
+router.post("/", addComment);
 
 export default router;
--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ import { addPosts, getPosts } from "../controllers/post.js";
 
 const router = express.Router();
 
-router.get("", getPosts);
-router.post("", addPosts);
+router.get("/", getPosts);
+router.post("/", addPosts);
 
 export default router;
